7887d64910c107544ab39eeb38a1c747b40b1bb6
Dimitri Belopopsky <dimitri@belopopsky.com>
2017-05-14 17:35:20 +0200

    Premier commit

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -7,40 +7,95 @@ void ofApp::setup(){
 
   ofSetVerticalSync(true);
 
-
-
   soundStream.printDeviceList();
-
 
   left.assign(bufferSize, 0.0);
   right.assign(bufferSize, 0.0);
 
-  soundBuffer.setNumChannels(2);
+
+  //ofSoundStreamSettings settings;
+  /*
+  auto devices = soundStream.getDeviceList();
+  settings.setOutDevice(devices[0]);
+
+  // or get the default device for an specific api:
+  //
+  // settings.api = ofSoundDevice::Api::PULSE;
+
+  // or by name:
+  //
+  //	auto devices = soundStream.getMatchingDevices("default");
+  //	if(!devices.empty()){
+  //		settings.setOutDevice(devices[0]);
+  //	}
 
 
-  soundStream.setup(this, 2, 2, 44100, bufferSize, 4);
+  settings.setOutListener(this);
+  settings.sampleRate = 48000;
+  settings.numOutputChannels = 2;
+  settings.numInputChannels = 1;
+  settings.bufferSize = bufferSize;
+  soundStream.setup(settings);
+  */
 
+  soundStream.setup(this, 2, 1, 48000, 256, 4);
+
+  ofDirectory dir(".");
+  dir.allowExt("cereal");
+  dir.listDir();
+  numFiles = dir.getFiles().size();
+
+  std::cout << numFiles << "\n";
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-
+  if (rec) {
+    unique_lock<mutex> lock(audioMutex);
+    line.clear();
+    float i = 0;
+    while (i < TWO_PI) { // make a heart
+      float r = (2-2*sin(i) + sin(i)*sqrt(abs(cos(i))) / (sin(i)+1.4)) * -80;
+      float x = ofGetWidth()/2 + cos(i) * (r * ofRandom(5));
+      float y = ofGetHeight()/2 + sin(i) * r;
+      line.addVertex(ofVec2f(x, y));
+      i+=0.005*HALF_PI*0.5;
+    }
+    line.close(); // close the shape
+  }
 }
 
 void ofApp::audioIn(ofSoundBuffer& buffer) {
-  soundBuffer.append(buffer);
-  for (int i = 0; i < buffer.getNumFrames(); i++) {
-    left[i] = buffer.getSample(i, 0) * 0.5;
-    right[i] = buffer.getSample(i, 1) * 0.5;
+  if (rec) {
+    soundBuffers.back().append(buffer);
+    unique_lock<mutex> lock(audioMutex);
+    soundPolylines.push_back(line);
+    for (int i = 0; i < buffer.getNumFrames(); i++) {
+      left[i] = buffer.getSample(i, 0) * 0.5;
+      right[i] = buffer.getSample(i, 1) * 0.5;
+    }
   }
 }
 
 void ofApp::audioOut(ofSoundBuffer& buffer) {
-  if (play) {
+  /*
+  if (soundBuffers.size() > 0) {
+    std::cout << "Buffer size: " << soundBuffers.back().size() << " sound stream buff size " << soundStream.getBufferSize() << "\n";
+    std::cout << "num frames " << buffer.getNumFrames() << "\n";
+  }
+  */
+  if (play && bufferCounter < soundBuffers.back().size()/soundStream.getBufferSize() - 1) {
+
+    //std::cout << "Buffer counter: " << bufferCounter << "\n";
     for (int i = 0; i < buffer.getNumFrames(); i++) {
-      buffer.getSample(i, 0) = buffer.getSample(i, 1) = soundBuffer.getSample(i + bufferCounter * buffer.getNumFrames(), 0);
+      auto sample = soundBuffers.back().getSample(i + (bufferCounter) * buffer.getNumFrames(), 0);
+      buffer.getSample(i, 0) = sample;
+      buffer.getSample(i, 1) = sample;
     }
     bufferCounter++;
+  } else {
+    play = false;
+    bufferCounter = 0;
   }
 }
 
@@ -93,18 +148,66 @@ void ofApp::draw(){
 
   ofPopMatrix();
   ofPopStyle();
+
+  if (play) {
+    /*
+    ofPushMatrix();
+    ofSetColor(ofColor::blue);
+    ofTranslate(100, 100);
+    soundPolylines[bufferCounter].draw();
+    ofPopMatrix();
+    */
+  }
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
+  if (key == 'r') {
+    std::cout << "Rec\n";
+    if (!rec) {
+      soundBuffers.emplace_back();
+      soundPolylines.clear();
+    }
+    rec = !rec;
+  }
 
-  if (key == ' ') {
+  if (key == 'p') {
     if (play) {
       std::cout << "Not play\n";
     } else {
       std::cout << "Play\n";
     }
+
     play = !play;
+  }
+
+  if (key == 's') {
+    std::ofstream os(ofToDataPath("soundbuffer"+std::to_string(numFiles)+".cereal"), std::ios::binary);
+    cereal::PortableBinaryOutputArchive archive( os );
+    std::cout << "save\n";
+    archive( soundBuffers );
+
+    ofDirectory dir(".");
+    dir.allowExt("cereal");
+    dir.listDir();
+    numFiles = dir.getFiles().size();
+  }
+
+  if (key == 'l') {
+    bool found = false;
+    do {
+      auto path = ofToDataPath("soundbuffer"+std::to_string((int)ofRandom(0,numFiles+1))+".cereal");
+      std::ifstream os(path, std::ios::binary);
+      if (os.is_open()) {
+	found = true;
+	cereal::PortableBinaryInputArchive archive( os );
+	std::cout << "load\n";
+	std::vector<ofSoundBuffer2> buff;
+	archive( buff );
+	soundBuffers.swap(buff);
+      }
+
+    } while (!found);
   }
 }
 
--- src/ofApp.h
+++ src/ofApp.h
@@ -1,36 +1,61 @@
 #pragma once
 
 #include "ofMain.h"
+#include <cereal/types/vector.hpp>
+#include <cereal/types/memory.hpp>
+#include <cereal/archives/portable_binary.hpp>
+#include <fstream>
+#include "ofSoundStream.h"
 
-class ofApp : public ofBaseApp{
+class ofSoundBuffer2 : public ofSoundBuffer {
 
-	public:
-		void setup();
-		void update();
-		void draw();
+  friend class cereal::access;
 
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
+  template<typename Archive>
+    void serialize(Archive& archive) {
+    archive( buffer, channels, samplerate, tickCount, soundStreamDeviceID);
+  }
+};
 
-		void audioIn(ofSoundBuffer& buffer);
-		void audioOut(ofSoundBuffer& buffer);
 
-		ofSoundStream soundStream;
-		ofSoundBuffer soundBuffer;
+class ofApp : public ofBaseApp {
 
-		int bufferCounter = 0;
+ public:
+  void setup();
+  void update();
+  void draw();
 
-		bool play = false;
+  void keyPressed(int key);
+  void keyReleased(int key);
+  void mouseMoved(int x, int y );
+  void mouseDragged(int x, int y, int button);
+  void mousePressed(int x, int y, int button);
+  void mouseReleased(int x, int y, int button);
+  void mouseEntered(int x, int y);
+  void mouseExited(int x, int y);
+  void windowResized(int w, int h);
+  void dragEvent(ofDragInfo dragInfo);
+  void gotMessage(ofMessage msg);
 
-		std::vector<float> left;
-		std::vector<float> right;
+  void audioIn(ofSoundBuffer& buffer);
+  void audioOut(ofSoundBuffer& buffer);
+
+  ofSoundStream soundStream;
+  std::vector<ofSoundBuffer2> soundBuffers;
+
+  std::vector<ofPolyline> soundPolylines;
+
+  ofPolyline line;
+
+  int bufferCounter = 0;
+
+  bool play = false;
+  bool rec = false;
+
+  std::vector<float> left;
+  std::vector<float> right;
+
+  mutex audioMutex;
+
+  int numFiles = 0;
 };
